116f29a0b36d590942a1f6b1f5f868bb5994a149
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-05-24 23:11:37 -0300

    chore(initi): initial config with chakra ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.50'
+      }
+    }
+  },
+  colors: {
+    gray: {
+      "900": '#181823',
+      "800": '#1f2029',
+      "700": '#353646',
+      "600": '#4b4d63',
+      "500": '#616480',
+      "400": '#797d9a',
+      "300": '#9699b0',
+      "200": '#b3b5bc',
+      "100": '#d1d2dc',
+      "50": '#eeeef2',
+
+    }
+  }
+})
