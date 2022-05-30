41f32c85e08c3d13f8655ce52066cf8a583314d5
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-05-30 19:33:48 -0300

    feat(header): adding responsivity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "./Logo";
 import { NotificationNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { Search } from "./Search";
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
   return (
     <Flex 
       as="header"
@@ -17,14 +21,15 @@ export function Header() {
       align="center"
     >
       <Logo/>
-      <Search />
+      
+      {isWideVersion && <Search />}
 
       <Flex
         align="center"
         ml="auto"
       >
         <NotificationNav />
-        <Profile/>
+        <Profile showProfileData={!!isWideVersion}/>
       </Flex>
     </Flex>
   )
