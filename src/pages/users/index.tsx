1fc31c61d219b0f28b9e7b8c94727ebb1e5c7b1b
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-06-04 16:07:55 -0300

    feat(users): listing users on table

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,11 +23,28 @@ import Link from 'next/link';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { useQuery } from 'react-query';
 
+type User = {
+  email: string;
+  name: string;
+  createdAt: string;
+  id: string;
+};
+
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, error } = useQuery<User[]>('users', async () => {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
-    return data;
+
+    const users = data.users.map((user: User) => ({
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    }));
+
+    return users;
   });
 
   const isWideVersion = useBreakpointValue({
@@ -84,20 +101,22 @@ export default function UserList() {
                     </Tr>
                   </Thead>
                   <Tbody>
-                    <Tr>
-                      <Td px={['4', '4', '6']}>
-                        <Checkbox colorScheme="pink" />
-                      </Td>
-                      <Td>
-                        <Box>
-                          <Text fontWeight="bold">Guilherme Victor</Text>
-                          <Text fontSize="small" color="gray.300">
-                            guivictorcontato@gmail.com
-                          </Text>
-                        </Box>
-                      </Td>
-                      {isWideVersion && <Td>04 de Abril, 2022</Td>}
-                    </Tr>
+                    {data?.map((user) => (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="small" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    ))}
                   </Tbody>
                 </Table>
 
