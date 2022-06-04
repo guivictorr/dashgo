268710a7ca6ccf809ea287430597df9afaf5ad5b
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-06-04 14:13:16 -0300

    feat(api): configuring miragejs to have mocked routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { SIdebarDrawerProvider } from 'context/SidebarDrawerContext'
 import { AppProps } from 'next/app'
+import { makeServer } from 'services/miragejs'
 import { theme } from 'styles/theme'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
